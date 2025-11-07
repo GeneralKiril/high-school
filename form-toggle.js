ac8717d93719325fe0447bfa6a51daa1957a9a51
GeneralKiril <kiril.georgiev.popov@gmail.com>
2025-11-07 11:52:52 +0200

    dadss

--- form-toggle.js
+++ form-toggle.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const contactForm = document.getElementById("contact-form");
     const emailBtn = document.getElementById("email-btn");
     const closeBtn = document.getElementById("close-btn");
+    const form = document.getElementById("emailForm");
 
     emailBtn.addEventListener("click", () => {
         mainContent.style.display = "none";
@@ -12,5 +13,20 @@ document.addEventListener("DOMContentLoaded", () => {
     closeBtn.addEventListener("click", () => {
         contactForm.style.display = "none";
         mainContent.style.display = "grid";
+    });
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const userName = encodeURIComponent(document.getElementById("name-input").value);
+        const subjectTopic = encodeURIComponent(document.getElementById("subject-input").value);
+        const message = encodeURIComponent(document.getElementById("message-input").value);
+
+        const subject = `Message%20from%20${userName}%20-%20${subjectTopic}`;
+        const body = `${message}`;
+
+        const mailtoLink = `mailto:fda.et.official@gmail.com?subject=${subject}&body=${body}`;
+
+        window.location.href = mailtoLink;
     });
 });
